1a3e6efca2ab295bd18577d00867aacb676839f2
lyaneii <146889907+lyaneii@users.noreply.github.com>
2024-12-02 10:28:23 +0100

    Add day3 template

---
+++ Day3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace day2
+{
+	public static class Program
+	{
+		private static void PartOne(string[] input)
+		{
+			long total = 0;
+
+			Console.WriteLine(total);
+		}
+
+		private static void PartTwo(string[] input)
+		{
+			long total = 0;
+
+			Console.WriteLine(total);
+		}
+
+		public static void Main(string[] args)
+		{
+			var lines = File.ReadAllLines("../../../input.txt");
+
+			Console.Write("Part One: ");
+			PartOne(lines);
+			Console.Write("Part Two: ");
+			PartTwo(lines);
+		}
+	}
+}
