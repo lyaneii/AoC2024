91a6a80c32ac3717bd14773e919cb3c084d8b55f
lyaneii <146889907+lyaneii@users.noreply.github.com>
2024-12-03 10:32:43 +0100

    Solved Day3 - 01 + 02

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,31 +1,61 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace day3
 {
 	public static class Program
 	{
-		private static void PartOne(string[] input)
+		private static void PartOne(string input)
 		{
 			long total = 0;
 
+			const string pattern = @"(?<=mul\()(\d+,\d+)(?=\))";
+
+			MatchCollection matches = Regex.Matches(input, pattern);
+
+			foreach (Match match in matches)
+			{
+				var pair = match.Groups[0].Value.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+				total += Convert.ToInt64(pair[0]) * Convert.ToInt64(pair[1]);
+			}
 			Console.WriteLine(total);
 		}
 
-		private static void PartTwo(string[] input)
+		private static void PartTwo(string input)
 		{
 			long total = 0;
+			
+			const string pattern = @"do\(\)|(?<=mul\()(\d+,\d+)(?=\))|don't\(\)";
 
+			MatchCollection matches = Regex.Matches(input, pattern);
+
+			bool doMul = true;
+			foreach (Match match in matches)
+			{
+				if (match.Groups[0].Value == "do()")
+					doMul = true;
+				else if (match.Groups[0].Value == "don't()")
+					doMul = false;
+				else if (doMul)
+				{
+					var pair = match.Groups[0].Value.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+					total += Convert.ToInt64(pair[0]) * Convert.ToInt64(pair[1]);
+				}
+			}
 			Console.WriteLine(total);
 		}
 
 		public static void Main(string[] args)
 		{
 			var lines = File.ReadAllLines("../../../input.txt");
+			var input = string.Concat(lines);
+
+			const string pattern = @"do\(\)|(?<=mul\()(\d+,\d+)(?=\))|don't\(\)";
 
 			Console.Write("Part One: ");
-			PartOne(lines);
+			PartOne(input);
 			Console.Write("Part Two: ");
-			PartTwo(lines);
+			PartTwo(input);
 		}
 	}
 }
